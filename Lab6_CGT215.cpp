1708dcc2f063340be148f4f85972c315b8cd80a6
Griffin Overmyer <griffinovermyer@gmail.com>
2023-09-20 11:16:27 -0400

    Present yoda on winter background with image merging

--- Lab6_CGT215.cpp
+++ Lab6_CGT215.cpp
@@ -2,19 +2,63 @@
 //
 
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
-int main()
-{
-    std::cout << "Hello World! Add this as a change\n";
+using namespace sf;
+using namespace std;
+
+int main() {
+	string background = "images1/backgrounds/winter.png";
+	string foreground = "images1/characters/yoda.png";
+	Texture backgroundTex;
+
+	if (!backgroundTex.loadFromFile(background)) {
+		cout << "Couldn't Load Image" << endl;
+		exit(1);
+	}
+	Texture foregroundTex;
+	if (!foregroundTex.loadFromFile(foreground)) {
+		cout << "Couldn't Load Image" << endl;
+		exit(1);
+	}
+
+	Image backgroundImage;
+	backgroundImage = backgroundTex.copyToImage();
+	Image foregroundImage;
+	foregroundImage = foregroundTex.copyToImage();
+	Vector2u sz = backgroundImage.getSize();
+	
+	//foreground will be the manipulated image
+	Color greenScreen;
+	for (int y = 0; y < sz.y; y++) {
+		for (int x = 0; x < sz.x; x++) {
+			//if pixel is not totally green use background pixel
+			if (y == 0 && x == 0) {
+				greenScreen = foregroundImage.getPixel(x, y);
+			}
+			Color currPixelBackground = backgroundImage.getPixel(x,y);
+			Color currPixelForeground = foregroundImage.getPixel(x, y);
+
+			if (currPixelForeground == greenScreen) {
+				//use background pixel
+				foregroundImage.setPixel(x, y, currPixelBackground);
+			}
+			//else keep foreground pixels that show yoda
+
+		}
+	}
+	
+	// By default, just show the foreground image
+	
+	RenderWindow window(VideoMode(1024, 768), "Here's the output");
+	Sprite sprite1;
+	Texture tex1;
+	
+	tex1.loadFromImage(foregroundImage);
+	sprite1.setTexture(tex1);
+	
+	window.clear();
+	window.draw(sprite1);
+	window.display();
+	while (true);
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
